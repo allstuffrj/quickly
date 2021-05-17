d8649d86a2210f5fc980c64b507bbb8d47c66420
Rahul <rahul@netsolitsolution.com>
2021-05-17 18:00:01 +0530

    Update

---
+++ src/Services/Notifications.ts
@@ -0,0 +1,10 @@
+import http from '../Config/HttpClient';
+
+class NotificationService {
+    getAll(): Promise<any> {
+      return http.get("/notification.json");
+    }
+  }
+  
+  export default new NotificationService();
+  
